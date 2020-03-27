3280b7857812074bf960b5908d249f9d1cd31131
atomicapple0 <brianzhangaa@gmail.com>
2020-03-27 19:02:46 -0400

    first commit

---
+++ Painter.java
@@ -0,0 +1,110 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
+
+@SuppressWarnings("serial")
+public class Painter extends JPanel{
+public ArrayList<Point> points;
+public boolean write;
+public boolean released = false;
+public boolean click = false;
+
+public Painter(ArrayList<Point> points, String mode) {
+    this.points = points;
+    this.write = mode.compareTo("w") == 0;
+
+    addMouseListener(new MouseAdapter() {
+        public void mousePressed(final MouseEvent event) {
+            if (!click){
+                click = true;
+            }
+            if (write){
+                points.add(event.getPoint());
+                repaint();
+            }
+        }
+    });
+
+    addMouseListener(new MouseAdapter() {
+        public void mouseReleased(final MouseEvent event) {
+            released = true;
+        }
+    });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+        public void mouseDragged(final MouseEvent event) {
+            if (write){
+                points.add(event.getPoint());
+                repaint();
+            }
+        }
+    });
+
+}
+
+public void paintComponent(final Graphics g) {
+    super.paintComponent(g);
+    final Graphics2D g2 = (Graphics2D) g;
+    //g2.setColor(new Color(0, 0, 128));
+    //g2.setStroke(new BasicStroke(8));
+    for (int i = 0; i < points.size(); i++){
+        g2.draw(new Rectangle2D.Float(points.get(i).x -2, points.get(i).y -2, 4, 4));
+    }
+    for (int i = 1; i < points.size(); i++){
+        g2.draw(new Line2D.Float(points.get(i-1), points.get(i)));
+    }
+}
+
+public static void sleep(int i){
+    try {
+        Thread.sleep(100);
+    }
+    catch (InterruptedException iex) {
+        iex.printStackTrace();
+    }
+}
+
+public static ArrayList<Point> drawPoints(){
+    final JFrame f = new JFrame();
+
+    ArrayList<Point> points = new ArrayList<Point>();
+    Painter HI = new Painter(points, "w");
+
+    f.add(HI);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setSize(1000, 1000);
+    f.setVisible(true);
+
+    while(!HI.released){
+        sleep(100);
+    }
+    f.dispose();
+    
+    return(HI.points);
+}
+
+public static ArrayList<Point> displayPoints(ArrayList<Point> points){
+    final JFrame f = new JFrame();
+
+    Painter HI = new Painter(points, "r");
+
+    f.add(HI);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setSize(1000, 1000);
+    f.setVisible(true);
+
+    while(!HI.click){
+        sleep(100);
+    }
+    sleep(100);
+    f.dispose();
+    
+    return(HI.points);
+}
+
+
+}
---
+++ Template.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Template {
+
+	public static void main(String[] args) {
+
+		Point p1 = new Point(1,2);
+		Point p2 = new Point(11,21);
+
+		try {
+			FileOutputStream f = new FileOutputStream(new File("myObjects.txt"));
+			ObjectOutputStream o = new ObjectOutputStream(f);
+
+			// Write objects to file
+			o.writeObject(p1);
+			o.writeObject(p2);
+
+			o.close();
+			f.close();
+
+			FileInputStream fi = new FileInputStream(new File("myObjects.txt"));
+			ObjectInputStream oi = new ObjectInputStream(fi);
+
+			// Read objects
+			Point pr1 = (Point) oi.readObject();
+			Point pr2 = (Point) oi.readObject();
+
+			System.out.println(pr1.toString());
+			System.out.println(pr2.toString());
+
+			oi.close();
+			fi.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Error initializing stream");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
